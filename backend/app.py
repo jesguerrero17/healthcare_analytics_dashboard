c99314c9387a4958e457d758778c6875e2650a95
jesguerrero17 <guerrero.a.jesus08@gmail.com>
2026-03-21 20:58:03 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -22,6 +22,9 @@ app.register_blueprint(predict_bp, url_prefix="/api")
 app.register_blueprint(risk_bp, url_prefix="/api")
 app.register_blueprint(summary_bp, url_prefix="/api")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
