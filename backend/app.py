5b01c36856a2a61a61b79268fcf5933fc6d1f60a
jesguerrero17 <guerrero.a.jesus08@gmail.com>
2026-03-22 20:29:08 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ model_path = os.path.join(os.path.dirname(__file__), "models", "cost_model.pkl")
 model = joblib.load(model_path)
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://your-frontend.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "https://healthcare-analytics-dashboard-one.vercel.app"}})
 
 app.config["MODEL"] = model
 
